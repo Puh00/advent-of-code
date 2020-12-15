35f2d71a96e6e9947599125a7d6737a197352fb4
Puh00 <alex.phu@live.com>
2020-12-16 00:01:08 +0100

    Day 8 part 2

--- day8/day8.java
+++ day8/day8.java
@@ -2,34 +2,73 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 
-public class day8 {
+public class Day8 {
 
-  public int getVal(String s) {
+  /** Retrieves the number from the instruction */
+  private int getVal(String s) {
     String inp = s.trim();
     char op = inp.charAt(0);
     int val = Integer.parseInt(inp.substring(1, inp.length()));
     return op == '+' ? val : -val;
   }
 
-  public static void main(String[] args) throws IOException {
-    String filepath = "/Users/nam/dev/advent-of-code-2020/day8/input.txt";
-    day8 dy = new day8();
-    List<String> input = Files.readAllLines(Path.of(filepath));
-    Set<Integer> index = new HashSet<>();
-
-    int acc = 0;
+  /**
+   * Returns if the run was successful, the final value of the accumulator, and a set of the visited
+   * instruction's addresses
+   */
+  private Truple<Boolean, Integer, Set<Integer>> runInstructions(List<String> input) {
+    Set<Integer> visitedInstructions = new HashSet<>();
+    Integer acc = 0;
     int i = 0;
-    while (!index.contains(i)) {
-      index.add(i);
-      String[] temp = input.get(i).split(" ");
-      String inst = temp[0];
-      int val = dy.getVal(temp[1]);
-      if (inst.equals("jmp")) {
+    while (!visitedInstructions.contains(i) && i < input.size()) {
+      visitedInstructions.add(i);
+      String[] instruction = input.get(i).split(" ");
+      String opCode = instruction[0];
+      int val = getVal(instruction[1]);
+      if (opCode.equals("jmp")) {
         i += val;
         continue;
-      } else if (inst.equals("acc")) acc += val;
+      } else if (opCode.equals("acc")) acc += val;
       i++;
     }
-    System.out.println("Part 1: " + acc);
+    Boolean isSuccessfulRun = i >= input.size();
+    return new Truple<Boolean, Integer, Set<Integer>>(isSuccessfulRun, acc, visitedInstructions);
+  }
+
+  /** Change the visited instructions (jmp/nop) and see if it resolves the infinite loop */
+  public Integer restoreInstructions(List<String> input) {
+    Set<Integer> visitedIns = runInstructions(input).z;
+    for (Integer k : visitedIns) {
+      String[] op = input.get(k).split(" ");
+      if (op[0] != "acc") {
+        String newOp = "nop " + op[1];
+        List<String> temp = new ArrayList<>(input);
+        temp.set(k, newOp);
+        Truple<Boolean, Integer, Set<Integer>> trup = runInstructions(temp);
+        // Successfully went through the last instruction
+        if (trup.x) return trup.y;
+      }
+    }
+    return -1;
+  }
+
+  private class Truple<X, Y, Z> {
+    public final X x;
+    public final Y y;
+    public final Z z;
+
+    public Truple(X x, Y y, Z z) {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    String filepath = "input.txt";
+    Day8 dy = new Day8();
+    List<String> input = Files.readAllLines(Path.of(filepath));
+    Integer acc = dy.restoreInstructions(input);
+    System.out.println("The final value of the accumulator: " + acc);
   }
 }
