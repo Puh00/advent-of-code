40c1ee4c0f96fab6050bc7144b5db05b5f1084e9
Puh00 <alex.phu@live.com>
2020-12-18 16:58:51 +0100

    Day 12 part 2

--- day12/Day12.java
+++ day12/Day12.java
@@ -4,61 +4,97 @@ import java.util.*;
 
 public class Day12 {
 
+  /** To simplify the calculations of the coordinates */
   enum Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
+    NORTH(0, 1),
+    EAST(1, 0),
+    SOUTH(0, -1),
+    WEST(-1, 0);
+
+    int xDir;
+    int yDir;
+
+    Direction(int xDir, int yDir) {
+      this.xDir = xDir;
+      this.yDir = yDir;
+    }
   }
 
-  // Rotate by intervals of 90 degrees clockwise
+  /** Rotate by intervals of 90 degrees, clockwise */
   public Direction rotateClockWise(Direction ord, int n) {
     return Direction.values()[Math.floorMod((ord.ordinal() + n), 4)];
-  }
-
-  public int manhattanDistance(Map<String, Integer> mp) {
-    int xCord = Math.abs(mp.getOrDefault("N", 0) - mp.getOrDefault("S", 0));
-    int yCord = Math.abs(mp.getOrDefault("E", 0) - mp.getOrDefault("W", 0));
-    return xCord + yCord;
   }
 
   public static void main(String[] args) throws IOException {
     Day12 dy = new Day12();
     List<String> input = Files.readAllLines(Path.of("input.txt"));
+    boolean runPartB = true;
 
     Direction dir = Direction.EAST;
-    Map<String, Integer> pos = new HashMap<>();
+    // Ship coordinates
+    int sx = 0;
+    int sy = 0;
+    // Waypoint coordinates
+    int wx = 10;
+    int wy = 1;
+
     for (String s : input) {
       char op = s.charAt(0);
       int val = Integer.parseInt(s.substring(1, s.length()));
-      switch (op) {
-        case 'N':
-          pos.put("N", pos.getOrDefault("N", 0) + val);
-          break;
-        case 'S':
-          pos.put("S", pos.getOrDefault("S", 0) + val);
-          break;
-        case 'E':
-          pos.put("E", pos.getOrDefault("E", 0) + val);
-          break;
-        case 'W':
-          pos.put("W", pos.getOrDefault("W", 0) + val);
-          break;
-        case 'L':
-          dir = dy.rotateClockWise(dir, -(val / 90));
-          System.out.println("changed dir: " + dir);
-          break;
-        case 'R':
-          dir = dy.rotateClockWise(dir, (val / 90));
-          System.out.println("changed dir: " + dir);
-          break;
-        case 'F':
-          String d = dir.toString().substring(0, 1);
-          pos.put(d, pos.getOrDefault(d, 0) + val);
-          break;
+      if (!runPartB) {
+        // Part A
+        switch (op) {
+          case 'N': sy += val; break;
+          case 'S': sy -= val; break;
+          case 'E': sx += val; break;
+          case 'W': sx -= val; break;
+          case 'L':
+            dir = dy.rotateClockWise(dir, -(val / 90));
+            break;
+          case 'R':
+            dir = dy.rotateClockWise(dir, (val / 90));
+            break;
+          case 'F':
+            sx += dir.xDir * val;
+            sy += dir.yDir * val;
+            break;
+          default:
+            throw new IllegalArgumentException("Invalid op code");
+        }
+      } else {
+        // Part B
+        switch (op) {
+          case 'N': wy += val; break;
+          case 'S': wy -= val; break;
+          case 'E': wx += val; break;
+          case 'W': wx -= val; break;
+          case 'L':
+            for (int i = 0; i < val / 90; i++) {
+              // Old x-axis becomes the new y-axis,
+              // old y-axis becomes the new x-axis but the inverse
+              int temp = wy;
+              wy = wx;
+              wx = temp * -1;
+            }
+            break;
+          case 'R':
+            for (int i = 0; i < val / 90; i++) {
+              // Old x-axis becomes the new y-axis but the inverse
+              // old y-axis becomes the new x-axis
+              int temp = wy;
+              wy = wx * -1;
+              wx = temp;
+            }
+            break;
+          case 'F':
+            sx += wx * val;
+            sy += wy * val;
+            break;
+          default:
+            throw new IllegalArgumentException("Invalid op code");
+        }
       }
     }
-    System.out.println(pos);
-    System.out.println(dy.manhattanDistance(pos));
+    System.out.println("Manhattan distance: " + (Math.abs(sx) + Math.abs(sy)));
   }
 }
