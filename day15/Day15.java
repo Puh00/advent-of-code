c1695a9a4d497d211c4b13a3675ba24c3cf3fae1
Puh00 <alex.phu@live.com>
2020-12-20 14:49:56 +0100

    🏎  8s solution...

--- day15/Day15.java
+++ day15/Day15.java
@@ -5,23 +5,24 @@ import java.util.*;
 public class Day15 {
 
   public static void main(String[] args) throws IOException {
-    String input = Files.readString(Path.of("/Users/nam/dev/advent-of-code-2020/day15/input.txt"));
+    String input = Files.readString(Path.of("input.txt"));
     String[] nums = input.trim().split(",");
 
     // <Number, Index>
     Map<Integer, Integer> cache = new HashMap<>();
 
+    // Change number to 2020 or 30000000
+    int number = 30000000;
     int i = 0;
     int prev = -1;
 
-    while (i < 2020) {
+    while (i < number) {
       if (i < nums.length - 1) {
         cache.put(Integer.parseInt(nums[i]), i);
-      } else if (i == nums.length - 1)
-        prev = Integer.parseInt(nums[i]);
+      } else if (i == nums.length - 1) prev = Integer.parseInt(nums[i]);
       else {
-        int tempPrev = cache.get(prev) == null ? 0 : (i-1) - cache.get(prev);
-        cache.put(prev, i-1);
+        int tempPrev = cache.get(prev) == null ? 0 : (i - 1) - cache.get(prev);
+        cache.put(prev, i - 1);
         prev = tempPrev;
       }
       i++;
